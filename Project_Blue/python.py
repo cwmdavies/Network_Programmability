31a12946977879f1ae727e257cefe97d3a8e2258
Christopher Davies <chris.davies@weavermanor.co.uk>
2018-05-16 15:15:50 +0100

    Correct error in script

--- Project_Blue/python.py
+++ Project_Blue/python.py
@@ -2,10 +2,10 @@
 from jinja2 import Environment, FileSystemLoader, Template
 import yaml
 
-ENV = Environment(loader=FileSystemLoader('Git\\\Projects\\Project_Blue\\Jinja'))
+ENV = Environment(loader=FileSystemLoader('Git\\Projects\\Project_Blue\\Jinja'))
 
 template = ENV.get_template('template.j2')
 
-with open('Git\\\Projects\\Project_Blue\\Yaml\\data.yml') as f:
+with open('Git\\Projects\\Project_Blue\\Yaml\\data.yml') as f:
     interfaces = yaml.load(f)
     print(template.render(interface_list=interfaces))
