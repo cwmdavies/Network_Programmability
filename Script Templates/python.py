ea1debcfabd4b59bcd73ccaa65f2754f77c9ef28
Christopher Davies <chris.davies@weavermanor.co.uk>
2018-05-16 14:54:50 +0100

    Changes to the directory

--- Script Templates/python.py
+++ Script Templates/python.py
@@ -2,10 +2,10 @@
 from jinja2 import Environment, FileSystemLoader, Template
 import yaml
 
-ENV = Environment(loader=FileSystemLoader('C:\\Stuff\\Python\\Git\\Templates\\Templates'))
+ENV = Environment(loader=FileSystemLoader('Git\\Templates\\Script Templates'))
 
-template = ENV.get_template("template.j2")
+template = ENV.get_template('template.j2')
 
-with open("C:\\Stuff\\Python\\Git\\Templates\\Templates\\data.yml") as f:
+with open('Git\\Templates\\Script Templates\\data.yml') as f:
     interfaces = yaml.load(f)
     print(template.render(interface_list=interfaces))
