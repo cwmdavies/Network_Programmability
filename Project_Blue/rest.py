05ff4e356f5b501f2a2f833db91c1ceb9ec32ce6
Christopher Davies <chris.davies@weavermanor.co.uk>
2018-05-17 16:09:13 +0100

    Added restconf script

---
+++ Project_Blue/rest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+
+    auth = HTTPBasicAuth('cisco', 'cisco')
+    headers = {'Accept': 'application/vnd.yang.data+json',
+    'Content-Type': 'application/vnd.yang.data+json'
+    }
+url = 'http://10.1.1.11/restconf/api/config/native?deep'
+response = requests.get(url, headers=headers, auth=auth)
+
+response = json.loads(response.text)
+print(json.dumps(response, indent=4))
