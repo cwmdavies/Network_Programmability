e770454d895f834845a83c664b042250f066b882
Christopher Davies <chris.davies@weavermanor.co.uk>
2018-05-18 11:15:56 +0100

    Structure Changes

--- Project_Netconf/python.py
+++ Project_Netconf/python.py
@@ -3,10 +3,10 @@
 from jinja2 import Environment, FileSystemLoader, Template
 import yaml
 
-ENV = Environment(loader=FileSystemLoader('Git\\Projects\\Project_Blue\\Jinja_Files'))
+ENV = Environment(loader=FileSystemLoader('Git\\Projects\\Project_Netconf\\Jinja_Files'))
 
 template = ENV.get_template('template.j2')
 
-with open('Git\\Projects\\Project_Blue\\Yaml_Files\\data.yml') as f:
+with open('Git\\Projects\\Project_Netconf\\Yaml_Files\\data.yml') as f:
     interfaces = yaml.load(f)
     print(template.render(interface_list=interfaces))
--- Project_Rest/rest.py
+++ Project_Rest/rest.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+import sys
 import json
 import requests
 from requests.auth import HTTPBasicAuth
