ceee5e8263b8fd279823b72c26da0640147a0b27
root <root@localhost.localdomain>
2018-05-25 14:05:28 +0100

    Code Changes

--- Project_Restconf/Get_Config - OSPF.py
+++ Project_Restconf/Get_Config - OSPF.py
@@ -17,4 +17,4 @@ json_response = json.dumps(response, indent=4)
 
 with open('Projects/Project_Restconf/Outputs/OSPF_Config.txt', 'w') as f:
     for capability in json_response:
-        f.write(str(capability))
+        f.write(str(capability)) 
