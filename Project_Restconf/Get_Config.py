e2849f886b1574e650dc91c47823c51334d1be26
cdavies <chris.davies@weavermanor.co.uk>
2018-06-04 12:50:31 +0100

    Change Permissions

--- Project_Restconf/Get_Config.py
+++ Project_Restconf/Get_Config.py
@@ -5,11 +5,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth('cisco', 'cisco')
+    auth = HTTPBasicAuth('root', 'D_Vay!_10&')
     headers = {'Accept': 'application/vnd.yang.data+json',
     'Content-Type': 'application/vnd.yang.data+json'
     }
-url = 'http://10.1.1.11/restconf/api/config/native?deep'
+url = 'http://ios-xe-mgmt.cisco.com/restconf/api/config/native?deep'
 response = requests.get(url, headers=headers, auth=auth)
 
 response = json.loads(response.text)
