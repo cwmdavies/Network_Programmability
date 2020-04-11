1a21dfe1bc735ef9868fb2aaa7ed9a898d1282a7
Christopher Davies <cdavies@localhost.localdomain>
2020-04-11 20:43:55 +0100

    minor changes

--- Project_Netconf/device_info.py
+++ Project_Netconf/device_info.py
@@ -1,3 +1,2 @@
-#! /usr/bin/env python
 ios_xe1 = {"address": "192.168.37.129", "netconf_port": 830, "restconf_port": 443, "ssh_port": 22, "username": "cisco", "password": "cisco", "device_type": "cisco_ios"}
 devnet1 = {"address": "ios-xe-mgmt.cisco.com", "netconf_port": 8181, "restconf_port": 9443 , "username": "root", "password": "D_Vay!_10&", "device_type": "cisco_ios"}
