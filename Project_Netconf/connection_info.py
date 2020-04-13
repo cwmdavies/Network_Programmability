a44b9bc81898bd8709dc6a36cb8c05510cbd3fa1
Christopher Davies <cdavies@localhost.localdomain>
2020-04-13 12:28:43 +0100

    Minor Changes

--- Project_Netconf/connection_info.py
+++ Project_Netconf/connection_info.py
@@ -1,6 +1,5 @@
-from ncclient import manager
 host = "192.168.2.250"
 port = 22
-username = "chris"
-password = "password"
+username = "cisco"
+password = "cisco"
 device_params={'name':"csr"}
--- Project_Netconf/get_capabilities.py
+++ Project_Netconf/get_capabilities.py
@@ -1,7 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from connection_info import host, port , username, password
 from ncclient import manager
-m = manager.connect(host=host,port=port,username=username,password=password,hostkey_verify=False,)
+m = manager.connect(host=host,
+                    port=port,
+                    username=username,
+                    password=password,
+                    hostkey_verify=False,)
 
 with open('Output\\capabilities.txt', 'w') as f:
     for capability in m.server_capabilities:
